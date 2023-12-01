6f1bfeb388243367e79de9df816f85598b336eda
MaryRamya11 <mary.ramya@goapptiv.com>
2023-12-01 10:32:27 +0530

    first commit

---
+++ constants.py
@@ -0,0 +1,5 @@
+
+class Constants:
+
+    Auth_Token = """Bearer AAAAx2FnNkc:APA91bEOSxdmrkk1ylnGuUKg-PcDmRslYwqrvG8iHjDJj-1nndbUGw3sEx_PsQBzraPKJA4fqosvi5lldTxYhsLT_SU0YcF0Y1tunoTCCrg3RoLnszzW2jBdwHtuiagzWWRNk66jawq0"""
+    
